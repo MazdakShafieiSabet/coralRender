8eb6a86366a4694ce746f84fe0538df86c9e187f
MazdakShafieiSabet <mazdaksabet1388@protonmail.com>
2026-04-21 15:02:45 +0200

    Made the code fully functional and finished README. Only need pictures now.

--- README.md
+++ README.md
@@ -11,18 +11,33 @@ This section shows how to install Blender and how to set it up properly to use w
 
 Firstly, grab the newest stable version of Blender and install it. 
 
-**Windows**
+## Windows
 <br>
 If you're on Windows go to the Blender homepage and click download. After that use the .msi or .msix file to install Blender. Note where it's being installed. If you already have it installed and you forgot where it is, it's typically installed in "C:\Program Files\Blender Foundation\Blender xx\" where xx in this context is the version number. Then hit the Windows-logo on your keyboard and search for "Environment". The search result you're looking for is called "Edit the system environment variables". Hit enter and look for the button "Environment Variables...". Click it and it'll take you to the page that lets you add a path to PATH (pun intended) Under "User Variables" find "Path", double-click it and select new. Now paste in the Blender install path you definitely copied. Press enter and close out of all of this. Now if you open a termial and type
 ```
 blender -v
 ```
-You should see an output. If you scroll a bit up to where the output begins, you see your Blender version. Yay, you added Blender to PATH!
+you should see an output. If you scroll a bit up to where the output begins, you see your Blender version. Yay, you added Blender to PATH!
 
-**Linux**
+## Linux
 <br>
 On Linux I recommend using the Flatpack or the Snap package as those (in my experience) default to shipping with OPTIX. You may also use the build coming in your package manager or even build one from source, I just recommend a different way. Anyway, Linux makes it easy on us since you do not need to add anything to PATH. Yay, you're basically done. And that kids, is why Linux is objectively better.
 
-**macOS**
+## macOS
 <br>
 Unfortunately, macOS is not currently a priority with this tool and it's a system I do not know well enough to develop for. It is in the future roadmap to add extensive Mac support. And if you're thinking "It's just a Python script, it cannot be that serious", I would have thought the same. But I have been dissapointed so often when trying to write and it works on Windows and Linux, but my friends can't run it cuz it's on a Mac. Right now the focus is to get a stable, working little app. That's all.
+
+# Prepping your scene
+## Step 1: Setting up
+First, make sure you've selected your preferred renderer (in my case this would be Cycles) and set your render settings appropriately. I recommend rendering one frame to see how it would look like before you commit to using this tool.
+Then make sure all your animation starts at frame 1 or a common start frame if you're doing a batch render. Otherwise this isn't important. Is this kind of annoying? Yes. Will I be working on this? Yes.
+Also make sure all files output to the same directory under the output settings in Blender if you plan on removing those outputs and only keeping the ones from your Compositor nodes.
+
+## Step 2: coralRender
+Enter the path to your file (or just the directory all your files are in) and enter the start frame (or leave at 1) and hit render. To remove the double outputs enter where the default Blender output is. Then hit "Start Render" and watch it go.
+
+# Future Roadmap
+- figure out if this even works on macOS
+- Flamenco incorporation
+- optimise code
+- add "Stop Render" button
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-import subprocess, threading
+import subprocess, threading, os
 
 ## GUI initialisation
 root = tk.Tk()
@@ -15,30 +15,72 @@ x = (screen_width/2) - (window_width/2)
 y = (screen_height/2) - (window_height/2)
 root.geometry('%dx%d+%d+%d' % (window_width, window_height, x, y))
 
+## Batch render logic
+def batch_render(frame_start):
+    batch = []  ## list of files to be rendered in batch mode
+    if not os.path.isdir(blend_file_entry.get()):   ## check if path is a directory
+        output_box.insert(tk.END, "Error: Batch mode requires a folder path, not a file.\n")
+        return
+    else:
+        for file in os.scandir(blend_file_entry.get()): ## scan through the provided directory for .blend files
+            if file.is_file() and file.name.endswith(".blend"):
+                batch.append(os.path.join(blend_file_entry.get(), file.name))
+        
+        for blend_file in batch:
+            cmd = ["blender", "-b", blend_file] ## command prepping
+            
+            if animation_var.get():
+                cmd += ["-s", frame_start, "-a"]    ## animation command
+            else:
+                cmd += ["-f", frame_start]   ## still frame command
+            
+            ## actual rendering, used to show output in the text box in the GUI
+            process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+            for line in process.stdout:
+                output_box.insert(tk.END, line)
+                output_box.see(tk.END)
+
+
 ## Render logic
 def run_render():
     blender_file = blend_file_entry.get()
     frame_start = frame_start_entry.get()
-
+    
     if frame_start == "":
-        frame_start = "1"  ## default to frame 1 if not specified
+        frame_start = "1"   ## default to frame 1 if not specified
     
-    cmd = ["blender", "-b", blender_file]
+    if batch_var.get(): ## check if batch render should be applied
+        batch_render(frame_start)
+    else:   ## single file render
+        cmd = ["blender", "-b", blender_file]   ## command prepping
+        
+        if animation_var.get():
+            cmd += ["-s", frame_start, "-a"]    ## animation command
+        else:
+            cmd += ["-f", frame_start]   ## still frame command
+        
+        ## actual rendering, used to show output in the text box in the GUI
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        for line in process.stdout:
+            output_box.insert(tk.END, line)
+            output_box.see(tk.END)
     
-    if animation_var.get():
-        cmd += ["-s", frame_start, "-a"]
-    else:
-        cmd += ["-f", frame_start]
-    
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-    for line in process.stdout:
-        output_box.insert(tk.END, line)
-        output_box.see(tk.END)
-
+    ## removing double exports, only needed when working with file output nodes in the compositer
+    if default_output_entry.get() != "" and file_output_var.get():
+        excess_output = default_output_entry.get()
+        try:
+            if os.path.isdir(excess_output): ## check if the provided path is a directory
+                for file in os.scandir(excess_output):
+                    if file.is_file():  ## check for files to delete
+                        os.remove(os.path.join(excess_output, file.name)) ## remove the files
+            else:
+                output_box.insert(tk.END, "Error: Default output path is not a valid directory.\n")
+        except Exception as e:
+            output_box.insert(tk.END, f"Error while trying to remove default output files: {e}\n")
 
 ## Labels
 main_label = tk.Label(root, text="coralRender", font=(32))
-blender_file_label = tk.Label(root, text="Path to .blend file:")
+blender_file_label = tk.Label(root, text="Path to .blend file (only add folder for batch rendering):")
 frame_start_label = tk.Label(root, text="Start Frame (default is 1):")
 default_output_label = tk.Label(root, text="Output Path to remove (can be left empty):")
 
@@ -51,7 +93,7 @@ default_output_entry = tk.Entry(root, width=25)
 file_output_var = tk.BooleanVar()
 animation_var = tk.BooleanVar()
 batch_var = tk.BooleanVar()
-file_output_only = ttk.Checkbutton( root, text='Only use File Output Nodes', variable=file_output_var)
+file_output_only_checkbox = ttk.Checkbutton( root, text='Only use File Output Nodes', variable=file_output_var)
 animation_checkbox = ttk.Checkbutton( root, text='Render Animation', variable=animation_var)
 batch_render_checkbox = ttk.Checkbutton( root, text='Batch Render', variable=batch_var)
 
@@ -63,28 +105,21 @@ start_render_button = tk.Button(root, text="Start Render", command=lambda: threa
 
 ## Gridding
 main_label.grid(row=0, column=0, columnspan=3, pady=10)
+
 blender_file_label.grid(row=1, column=0, sticky="e", padx=5, pady=5)
-blend_file_entry.grid(row=1, column=1, sticky="w", padx=5, pady=5)
 frame_start_label.grid(row=2, column=0, sticky="e", padx=5, pady=5)
-frame_start_entry.grid(row=2, column=1, sticky="w", padx=5, pady=5)
 default_output_label.grid(row=3, column=0, sticky="e", padx=5, pady=5)
+
+blend_file_entry.grid(row=1, column=1, sticky="w", padx=5, pady=5)
+frame_start_entry.grid(row=2, column=1, sticky="w", padx=5, pady=5)
 default_output_entry.grid(row=3, column=1, sticky="w", padx=5, pady=5)
 
 batch_render_checkbox.grid(row=2, column=2, sticky="w", padx=5, pady=5)
 animation_checkbox.grid(row=3, column=2, sticky="w", padx=5, pady=5)
-file_output_only.grid(row=4, column=2, sticky="w", padx=5, pady=5)
+file_output_only_checkbox.grid(row=4, column=2, sticky="w", padx=5, pady=5)
 
 output_box.grid(row=6, column=0, columnspan=3, padx=5, pady=5)
 
 start_render_button.grid(row=7, column=0, columnspan=3, pady=10)
 
-
 root.mainloop()
-
-## print("This is an automated render script meant to be used to render out images and image sequences from Blender.")
-## print("This script assumes you already have Blender installed and it is added to your system's PATH variable.")
-## print("If this is not the case, follow the instructions in the README file to set up Blender and add it to your PATH variable.")
-## 
-## blend_file = input("Enter the path to your .blend file (including the filename and extension): ")
-## output_path = input("Enter where the default Blender output is to remove it after rendering (if you don't have a File Output node in your compositor, you can just enter and the directory won't be deleted): ")
-
